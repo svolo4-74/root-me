a3028b6f1265b30edac3264b00e772295caaacdf
svolo4-74 <80594706+svolo4-74@users.noreply.github.com>
2023-10-05 07:49:03 -0400

    Update sec.php

--- sec.php
+++ sec.php
@@ -2,7 +2,7 @@
   echo("Success!\n");
   //$command = $_GET['cmd'];
   //echo $command."\n";
-  $infile = file_get_contents("secret.php");
+  $infile = file_get_contents("null.php");
   echo base64_encode($infile);
   //show_source(__FILE__);
 ?>
