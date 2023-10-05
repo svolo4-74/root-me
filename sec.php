e9f8fad86c63af834d908aaa62d6daafa7a9e333
svolo4-74 <80594706+svolo4-74@users.noreply.github.com>
2023-10-05 16:05:36 +0500

    Update and rename secret.php to sec.php

--- sec.php
+++ sec.php
@@ -2,7 +2,7 @@
   echo("Success!\n");
   //$command = $_GET['cmd'];
   //echo $command."\n";
-  $infile = file_get_contents("index.php");
+  $infile = file_get_contents("secret.php");
   echo base64_encode($infile);
   //show_source(__FILE__);
 ?>
